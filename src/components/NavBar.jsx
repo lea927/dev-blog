efe6fcd1b8dfd4a8469bb5d530090999c8980fec
Lea Tinoso <lxthym@icloud.com>
2025-06-10 17:43:34 +0800

    feat:o install react-icons, design navbar, add not found route/component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { FaRegEdit } from 'react-icons/fa'
+
 
 const NavBar = () => {
+  const navigate = useNavigate()
+
   return (
-    <nav>
-      <h1>DevBlog</h1>
-      <div>
+    <nav className='flex justify-between items-center p-5'>
+       <Link
+          to={{
+            pathname: "/",
+          }}
+          className='p-2 text-black hover:text-gray-600 font-bold'
+        >
+          DevBlog
+        </Link>
+      <div className="flex items-center gap-4">
         <Link
           to={{
             pathname: "/",
           }}
+          className='p-2 text-black hover:text-gray-600'
         >
           Home
         </Link>
@@ -17,9 +28,17 @@ const NavBar = () => {
           to={{
             pathname: "/about",
           }}
+          className='p-2 text-black hover:text-gray-600'
         >
           About
         </Link>
+        <button
+          onClick={() => navigate('/new')}
+          className='p-4 bg-black text-white rounded hover:opacity-80 flex items-center gap-2'
+        >
+           <FaRegEdit />
+          New Post
+        </button>
       </div>
     </nav>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,22 @@ import {
 } from "react-router-dom"
 import Home from "./routes/Home.jsx"
 import About from './routes/About.jsx'
+import NewPost from './routes/NewPost.jsx'
+import NotFound from './routes/NotFound.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <NotFound />,
   },
   {
     path: "/about",
     element: <About />,
+  },
+  {
+    path: "/new",
+    element: <NewPost />,
   }
 ]);
 
