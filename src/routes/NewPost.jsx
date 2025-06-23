e0c57bf256ae20fd38749b24fcac43fd6e42a2d7
Lea Tinoso <lxthym@icloud.com>
2025-06-23 19:01:00 +0800

    feat: designs new post page

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,6 +1,54 @@
+import { FaArrowLeft } from 'react-icons/fa'
+
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div className="flex justify-center items-center w-full h-screen">
+      <div className="w-8/12">
+        <div className="text-left w-full mb-8">
+          <p className="flex items-center gap-2">
+            <FaArrowLeft />
+            Back To Posts
+          </p>
+        </div>
+        <div className="border border-gray-200 px-6 py-8 rounded-md">
+          <h2 className="text-xl font-bold mb-4">Create New Blog Post</h2>
+          <div className="mb-4">
+            <p className="text-base font-semibold mb-1">Title</p>
+            <input
+              type="text"
+              placeholder="Enter post title"
+              className="border rounded-md px-3 py-2 w-full border-gray-200"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-base font-semibold">Cover Image URL</p>
+            <input
+              type="text"
+              placeholder="https://example.com/image.jpg"
+              className="border rounded-md px-3 py-2 w-full border-gray-200"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-base font-semibold">Content</p>
+            <textarea
+              placeholder="Write your post content here..."
+              className="border rounded-md px-3 py-2 w-full border-gray-200 h-80 mb-4"
+            ></textarea>
+            <div className="flex justify-end gap-1">
+              <button
+                className="border rounded-md border-gray-200 bg-transparent py-2 px-4"
+              >
+                Cancel
+              </button>
+              <input
+                className="border rounded-md py-2 px-4 text-white bg-black"
+                type="submit" value="Publish Post"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
