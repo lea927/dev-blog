509d760f115bfc8238c7de4d2ccbceb07d9d7a50
Lea Tinoso <lxthym@icloud.com>
2025-06-23 16:20:06 +0800

    feat: update blog card, home page, search box design

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const BlogCard = ({title, excerpt, date, imageUrl, readTime}) => {
   return (
     <>
-      <div className='max-w-[360px] h-[420px] rounded-md overflow-hidden'>
+      <div className='max-w-[360px] h-[420px] rounded-md overflow-hidden border border-gray-200'>
         <img src={imageUrl} alt="" className="w-full object-cover overflow-hidden" />
-        <h2 className='text-lg font-bold'>{title}</h2>
-        <p>{excerpt}</p>
-        <div className='flex justify-between'>
-          <p>{date}</p>
-          <p>{`${readTime} min read`}</p>
+        <div className='px-5 py-5'>
+          <h2 className='text-lg font-bold mb-2'>{title}</h2>
+          <p className='text-gray-600 mb-3'>{excerpt}</p>
+          <div className='flex justify-between text-gray-600 text-sm'>
+            <p>{date}</p>
+            <p>{`${readTime} min read`}</p>
+          </div>
         </div>
       </div>
     </>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -14,18 +14,18 @@ const Home = () => {
   return (
     <>
       <NavBar/>
-      <div className='flex flex-col items-center h-screen mx-4'>
-        <div>
+      <div className='flex flex-col items-center h-screen mx-4 gap-7'>
+        <div className=' w-full py-16 text-center'>
           <h1 className='text-4xl font-bold mb-4'>My Blog</h1>
-          <p className='text-lg'>Welcome to my personal blog where I share my thoughts on technology,
-  programming, and life as a developer.</p>
+          <p className='text-lg text-gray-600'><span className='block'>Welcome to my personal blog where I share my thoughts on technology,</span>
+  <span className='block'>programming, and life as a developer.</span></p>
         </div>
         <input
           type="text"
           value={search}
           onChange={handleSearchChange}
           placeholder="Search posts..."
-          className="border rounded px-3 py-2 w-64"
+          className="border rounded px-3 py-2 w-full border-gray-200"
         />
         <BlogList posts={blogPosts}/>
       </div>
